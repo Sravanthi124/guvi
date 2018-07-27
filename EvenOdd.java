357ece490e46c90323c5563e529e81baf03eb169
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2018-07-27 13:16:32 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class EvenOdd {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println("even");
+        else
+            System.out.println("odd");
+    }
+}
