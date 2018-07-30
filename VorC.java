89fd860e2b01a36aa36be613748a239f6970da41
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2018-07-30 10:17:25 +0530

    VorC.java

---
+++ VorC.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class VorC
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char c='a';
+		if(c=='a')
+		{
+			System.out.println("vowel");
+		}
+		else
+		{
+			System.out.println("consonant");
+		}
+	}
+}
