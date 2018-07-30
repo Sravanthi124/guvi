3751bffe9fb1838b3022899501b5830ffc5ad8de
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2018-07-30 10:44:41 +0530

    large.java

---
+++ large.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class large
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,b,c;
+		Scanner  in=new Scanner(System.in);
+		System.out.println("enter a value");
+		a=in.nextInt();
+		System.out.println("enter b value");
+		b=in.nextInt();
+		System.out.println("enter c value");
+		c=in.nextInt();
+		if(a>b&&a>c)
+		{
+		System.out.println("a is larger");	
+		}
+		else if(b>a&&b>c)
+		{
+			System.out.println("b is larger");
+		}
+		else
+		{
+		System.out.println("c is larger");	
+		}
+		
+		
+	}
+}
