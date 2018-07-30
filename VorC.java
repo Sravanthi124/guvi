0e35377be9e77a4b470b8ff5d7a2cf1b60356ca1
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2018-07-30 13:56:21 +0530

    Update VorC.java

--- VorC.java
+++ VorC.java
@@ -7,14 +7,20 @@ class VorC
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		char c='a';
-		if(c=='a')
+		scanner sc=new Scanner(System.in)
+		char c=sc.next().charAt(0);
+		if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
 		{
-			System.out.println("vowel");
+			System.out.println("Vowel");
+		}
+		else if(c>='a'&&c>='z'||c>='A'&&c>='Z')
+		{
+			System.out.println("Consonant");
 		}
 		else
 		{
-			System.out.println("consonant");
-		}
+			System.out.println("Invalid");
+		}	
+			
 	}
 }
