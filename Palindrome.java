b85df5bdb65a4bf81154f20b6bd418e19e68babb
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2018-07-31 14:28:52 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Palindrome
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int temp=n,sum=0,res;
+		  while(n>0)
+		  {    
+			 res=n%10;  //getting remainder  
+			 sum=(sum*10)+res;    
+			 n=n/10;    
+		  }    
+		if(temp==sum)    
+			System.out.println("yes");    
+	    else    
+			System.out.println("no");    
+	}
+}
+	
