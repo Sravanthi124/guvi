c888b5db8feb45c5026222d9557c7e8219746e62
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2018-08-08 12:51:33 +0530

    Create Amgs.java

---
+++ Amgs.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Amgs
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int st=sc.nextInt();
+		int e=sc.nextInt();
+		for (int k=st;k<=e;k++) 
+            	{ 
+                                int n=k; 
+                                int d=0; 
+                                int s=0;
+                                while(n>0) 
+                                 { 
+                                             d=n % 10; 
+                                             s=s+(d*d*d); 
+                                             n=n/10; 
+                                 } 
+                                 if(k==s) 
+                                  { 
+                                             System.out.println(k );
+                                  } 
+                      } 
+	}
+}
