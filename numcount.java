54c670afd7231ee14f86ee820ca3bc5cac1cde4f
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2018-08-11 12:47:40 +0530

    Create numcount.java

---
+++ numcount.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class numcount
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		int count=0;
+		for(int i=0;i<str.length();i++)
+		{
+			if(Character.isDigit(str.charAt(i)))
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
+			
+	}
+}
