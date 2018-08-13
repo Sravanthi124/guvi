cb640b1f8aed97f265f2fcdae48f6b63cc90f8ce
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2018-08-13 16:15:15 +0530

    Create mix.java

---
+++ mix.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class mix
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String sum;
+		String s1=sc.next();
+		String s2=sc.next();
+		sum=s1+s2;
+		System.out.println(sum);
+	}
+}
