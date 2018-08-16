49e477f08187579c1453365d72fbed3d2db07c80
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2018-08-16 10:12:10 +0530

    Create Binary.java

---
+++ Binary.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Binary
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int i;
+		int count=0;
+		Scanner sc=new Scanner(System.in);
+		String c=sc.next();
+		for(i=0;i<c.length();i++)
+		{
+			if(c.charAt(i)=='0'||c.charAt(i)=='1')
+			{
+				count++;
+			}
+		}
+		if(count==c.length())
+		{
+		System.out.print("yes");
+		}
+		else
+		{
+				System.out.print("no");
+			
+		}
+	}
+}
