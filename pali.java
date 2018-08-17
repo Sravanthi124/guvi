b5ca7acb6d19be09a4ad5165fccf1475fe4e343f
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2018-08-17 11:48:44 +0530

    Create pali.java

---
+++ pali.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class pali
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int i;
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		String s1="";
+		for(i=0;i<s.length();i++)
+		{
+			s1=s.charAt(i)+s1;
+		}
+		if(s.equals(s1))
+		{
+			System.out.print("yes");
+		}
+		else
+		{
+			System.out.print("no");
+		}
+	}
+}
