57e091c7e1874d6f3133b210b53704e5688fb15c
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2018-08-17 21:38:37 +0530

    Create float.java

---
+++ float.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class float
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		float f=sc.nextFloat();
+		int a=(int)(f);
+		a=a+1;
+		System.out.print(a);
+	}
+}
