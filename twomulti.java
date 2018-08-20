7729a13c0499097b7b2eb6973a486cf7afe8e100
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2018-08-20 11:23:05 +0530

    Create twomulti.java

---
+++ twomulti.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class twomulti
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n1;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		n1=n*2;
+		System.out.print(n1);
+	}
+
+}
