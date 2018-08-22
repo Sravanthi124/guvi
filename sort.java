3b892ced15b5094b255c422382ab65c5c99e5b88
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2018-08-22 10:41:59 +0530

    Create sort.java

---
+++ sort.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+import java.util.Arrays;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class sort
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n;
+		n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		Arrays.sort(a);
+		for(int i=0;i<n;i++)
+		{
+			if(i==n-1)
+			{
+				System.out.print(a[i]);
+			}
+			else
+			{
+				System.out.print(a[i]+" ");
+			}
+		}
+	}
+}
