87c5928d18d346d5bef931eda50739f6d357556f
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2018-08-23 10:37:25 +0530

    Create minhour.java

---
+++ minhour.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class minhour
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=n/60;
+		int h=n%60;
+		System.out.print(m+" "+h);
+	}
+}
