d1ee12d63ed8fe3bc10ec97577965f5df5565720
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2018-08-24 09:54:12 +0530

    Create celkal.java

---
+++ celkal.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class celkal
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int kel=0;
+		double cel=sc.nextInt();
+		kel=(int)(cel+273.5);
+		System.out.print(kel);
+	}
+}
