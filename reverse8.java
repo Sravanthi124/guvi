92d58fa742eaec5ab00a2beb831c227b17a71031
Sravanthi124 <41729390+Sravanthi124@users.noreply.github.com>
2018-08-24 11:17:28 +0530

    Create reverse8.java

---
+++ reverse8.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class reverse8
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int sum=0,rem=0;
+		int n=sc.nextInt();
+		while(n>0)
+		{
+			rem=n%10;
+			sum=sum*10+rem;
+			n=n/10;
+		}
+		System.out.print(sum);
+
+	}
+}
